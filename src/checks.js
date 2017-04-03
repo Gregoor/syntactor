31a2d3f3ab92cbbb7ade7c7552e58e98de676664
Gregor Weber <gregorwbr@gmail.com>
2017-04-03 22:18:23 +0200

    add missing type annotations

--- src/checks.js
+++ src/checks.js
@@ -1,22 +1,23 @@
 // @flow
+import type {ASTNode} from './types';
 
-export function isBooleanLiteral(node) {
+export function isBooleanLiteral(node: ASTNode) {
   return node.get('type') === 'BooleanLiteral';
 }
 
-export function isNumericLiteral(node) {
+export function isNumericLiteral(node: ASTNode) {
   return node.get('type') === 'NumericLiteral';
 }
 
-export function isStringLiteral(node) {
+export function isStringLiteral(node: ASTNode) {
   return node.get('type') === 'StringLiteral';
 }
 
-export function isArrayExpression(node) {
+export function isArrayExpression(node: ASTNode) {
   return node.get('type') === 'ArrayExpression';
 }
 
-export function isObjectExpression(node) {
+export function isObjectExpression(node: ASTNode) {
   return node.get('type') === 'ObjectExpression';
 }
 
