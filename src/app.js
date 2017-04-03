0627d8df2f856eb8c1ff124773a86e98b057a8bd
Gregor Weber <gregorwbr@gmail.com>
2017-04-03 22:18:28 +0200

    undo

--- src/app.js
+++ src/app.js
@@ -2,19 +2,21 @@
 import React, {PureComponent} from 'react';
 import * as Immutable from 'immutable';
 
-import type {ASTNode} from './types';
+import type {ASTNode, ASTPath} from './types';
 import {ArrayExpression, ObjectExpression} from './components/collections';
 import Keymap from './components/keymap';
 import {BooleanLiteral, NumericLiteral, StringLiteral} from './components/literals';
 import {
   isBooleanLiteral, isNumericLiteral, isArrayExpression, isObjectExpression, isEditable
 } from './checks';
-import example from './example.json';
+import example from '../package.json';
 import navigate from './navigate';
 import parse from './parse';
 import renderTypeElement, {injectTypeElements} from './render-type-element';
 
 const {List, Map} = Immutable;
+
+const MAX_HISTORY_LENGTH = 100;
 
 injectTypeElements({
   BooleanLiteral,
@@ -46,20 +48,22 @@ const cardStyle = {
 export default class App extends PureComponent {
 
   state = {
-    inputMode: false,
-    root: parse(example),
-    rootHistory: new List(),
-    selected: new List()
+    history: new List([
+      new Map({
+        inputMode: false,
+        root: parse(example),
+        selected: new List()
+      })
+    ])
   };
 
   getSelectedNode() {
-    const {root, selected} = this.state;
-    return root.getIn(selected);
+    const editorState = this.state.history.first();
+    return editorState.get('root').getIn(editorState.get('selected'));
   }
 
-  getClosestCollectionPath() {
-    const {selected} = this.state;
-    const selectedNode = this.getSelectedNode();
+  getClosestCollectionPath(root: ASTNode, selected: ASTPath) {
+    const selectedNode = root.getIn(selected);
 
     if (selectedNode) {
       if (isObjectExpression(selectedNode)) {
@@ -73,7 +77,7 @@ export default class App extends PureComponent {
     return selected.slice(0, index + 1);
   }
 
-  getDirection(key) {
+  getDirection(key: string) {
     switch (key) {
 
       case 'k':
@@ -92,20 +96,65 @@ export default class App extends PureComponent {
       case 'ArrowRight':
         return 'RIGHT';
 
+      default:
+        return;
+
     }
   }
 
+  updateHistory(updateFn: (root: ASTNode, selected: ASTPath) => any) {
+    this.setState(({history}) => {
+      const editorState = history.first();
+      const root = editorState.get('root');
+      const selected = editorState.get('selected');
+      const inputMode = editorState.get('inputMode');
+      return {
+        history: history.unshift(new Map({
+          inputMode, root, selected,
+          ...updateFn(root, selected, inputMode)
+        })).slice(0, MAX_HISTORY_LENGTH)
+      };
+    });
+  }
+
+  updateValue(updateFn: (any) => any) {
+    this.updateHistory((root, selected) => ({
+      root: root.updateIn(selected.push('value'), updateFn)
+    }));
+  }
+
+  insert = (node: ASTNode) => this.updateHistory((root, selected) => {
+    const collectionPath = this.getClosestCollectionPath(root, selected);
+    const itemIndex = selected.get(collectionPath.size) + 1 || 0;
+
+    const isArray = isArrayExpression(root.getIn(collectionPath.slice(0, -1)));
+
+    const newRoot = root.updateIn(collectionPath, (list) => list.insert(
+      itemIndex,
+      isArray
+        ? node
+        : new Map({type: 'ObjectProperty', key: StringNode, value: node})
+    ));
+    const newSelected = isArray
+      ? collectionPath.push(itemIndex)
+      : collectionPath.push(itemIndex, 'key');
+    return {
+      inputMode: !isArray || isEditable(newRoot.getIn(newSelected)),
+      root: newRoot,
+      selected: newSelected
+    };
+  });
+
   handleKeyDown = (event: any) => {
-    const {inputMode, root, selected} = this.state;
     const {ctrlKey, key} = event;
 
     const direction = this.getDirection(key);
 
-    if (!inputMode) {
+    if (!this.state.history.first().get('inputMode')) {
       event.preventDefault();
 
       if (direction) {
-        return this.setState(({root, selected}) => ({
+        return this.updateHistory((root, selected) => ({
           selected: navigate(direction, root, selected)
         }));
       }
@@ -134,22 +183,19 @@ export default class App extends PureComponent {
 
         case '+':
         case '-':
-          // TODO: history
-          return isNumericLiteral(selectedNode) && this.setState({
-              root: root.updateIn(selected.push('value'), (value) => value + (key === '+' ? 1 : -1))
-            });
+          return isNumericLiteral(selectedNode) && this.updateValue(
+            (value) => value + (key === '+' ? 1 : -1)
+          );
 
         case 't':
         case 'f':
-          // TODO: history
-          return isBooleanLiteral(selectedNode) && this.setState({
-              root: root.setIn(selected.push('value'), Boolean(key === 't'))
-            });
+          return isBooleanLiteral(selectedNode) && this.updateValue(
+            () => Boolean(key === 't')
+          );
 
         case 'd':
         case 'Delete':
-          // TODO: history
-          return this.setState(({
+          return this.updateHistory((root, selected) => ({
             root: root.deleteIn(
               selected.slice(0, 1 + selected.findLastIndex((value) => typeof value === 'number'))
             ),
@@ -158,7 +204,7 @@ export default class App extends PureComponent {
 
         case 'i':
         case 'Enter':
-          return isEditable(selectedNode) && this.setState(() => ({inputMode: true}));
+          return isEditable(selectedNode) && this.updateHistory(() => ({inputMode: true}));
 
         default:
 
@@ -166,57 +212,32 @@ export default class App extends PureComponent {
     }
 
     if (ctrlKey && key === 'z') {
-      return this.setState(({root, rootHistory}) => ({
-        root: rootHistory.last() || root,
-        rootHistory: rootHistory.pop()
+      return this.setState(({history}) => ({
+        history: history.slice(1)
       }));
     }
 
     if (['Enter', 'Escape'].includes(key)) {
-      return this.setState(({root, rootHistory, selected}) => {
-        const newRoot = root.getIn(selected.push('type')) === 'NumericLiteral'
+      return this.updateHistory((root, selected) => ({
+        inputMode: false,
+        root: root.getIn(selected.push('type')) === 'NumericLiteral'
           ? root.updateIn(selected.push('value'), (value) => parseFloat(value))
-          : root;
-        return ({
-          inputMode: false,
-          root: newRoot,
-          rootHistory: rootHistory.push(newRoot)
-        });
-      });
+          : root
+      }));
     }
   };
 
   handleChange = ({target: {value}}: any) => {
-    if (this.state.inputMode) this.setState(({root, selected}) => {
-      return {root: root.setIn(selected.push('value'), value)};
-    });
-  };
-
-  insert = (node: ASTNode) => {
-    const {root, selected} = this.state;
-    const collectionPath = this.getClosestCollectionPath();
-    const itemIndex = selected.get(collectionPath.size) + 1 || 0;
-
-    const isArray = isArrayExpression(root.getIn(collectionPath.slice(0, -1)));
-
-    const newRoot = root.updateIn(collectionPath, (list) => list.insert(
-      itemIndex,
-      isArray
-        ? node
-        : new Map({type: 'ObjectProperty', key: StringNode, value: node})
-    ));
-    const newSelected = isArray
-      ? collectionPath.push(itemIndex)
-      : collectionPath.push(itemIndex, 'key');
-    this.setState({
-      inputMode: !isArray || isEditable(newRoot.getIn(newSelected)),
-      root: newRoot,
-      selected: newSelected
-    });
+    this.updateHistory((root, selected, inputMode) => inputMode ? {
+      root: root.setIn(selected.push('value'), value)
+    } : {});
   };
 
   render() {
-    const {inputMode, root, selected} = this.state;
+    const {history} = this.state;
+    const editorState = history.first();
+    const inputMode = editorState.get('inputMode');
+    const selected = editorState.get('selected');
     const isInArray = (selected.last() === 'end' ? selected.slice(0, -2) : selected)
         .findLast((key) => ['elements', 'properties'].includes(key)) === 'elements';
     return (
@@ -246,7 +267,7 @@ export default class App extends PureComponent {
 
           <div style={{display: 'flex', flexDirection: 'row'}}>
             <form onChange={this.handleChange} style={{height: '100%', width: '100%', ...cardStyle}}>
-              {renderTypeElement(root, {inputMode, level: 0, selected})}
+              {renderTypeElement(editorState.get('root'), {inputMode, level: 0, selected})}
             </form>
             <div style={{marginLeft: 10, minWidth: 270, height: '100%', ...cardStyle}}>
               <Keymap {...{inputMode, isInArray}} selectedNode={this.getSelectedNode()}/>
