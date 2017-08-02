7e6b1c5878c753785b47c0cb5f7a179ff95de58f
Gregor Weber <gregorwbr@gmail.com>
2017-08-03 00:07:46 +0200

    include global browser version in index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {render} from './api';
 import Demo from './components/demo';
 
-ReactDOM.render(
-  <Demo />,
-  document.getElementById('root')
-);
+const demoElement = document.getElementById('insert-syntactor-demo-here');
+if (demoElement) {
+  ReactDOM.render(<Demo/>, demoElement);
+} else {
+  window.Syntactor = {render: render};
+}
