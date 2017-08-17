a92dc1c02cd2f922bf8f25eac7715b9cb850e030
Gregor Weber <gregorwbr@gmail.com>
2017-08-17 11:53:51 +0200

    update styled-components flow-typed

--- src/components/demo.js
+++ src/components/demo.js
@@ -18,24 +18,20 @@ const Head = styled.h1`
   ${styles.text}
   font-size: 2em;
   text-align: center;
-  
-  &:before, &:after {
-    color: #B7B7B7;
-  }
-  &:before {
-    content: "{";
-  }
-  &:after {
-    content: "}";
-  }
 `;
+
+const Symbol = styled.span`
+  color: #B7B7B7;
+`;
+
+const Brace = ({charCode}) => <Symbol>{String.fromCharCode(charCode)}</Symbol>;
 
 const Card = styled.div`
   padding: 20px;
   background: white;
-  box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),
-    0 3px 1px -2px rgba(0,0,0,.2),
-    0 1px 5px 0 rgba(0,0,0,.12);
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .14),
+    0 3px 1px -2px rgba(0, 0, 0, .2),
+    0 1px 5px 0 rgba(0, 0, 0, .12);
 `;
 
 const Nav = styled.div`
@@ -45,42 +41,38 @@ const Nav = styled.div`
   justify-content: space-between;
 `;
 
-const Symbol = styled.span`
-  color: grey;
-`;
-
 export default class Demo extends PureComponent {
 
   render() {
     return (
-        <div style={{maxWidth: 800, margin: '0 auto'}}>
-          <Head>Syntactor</Head>
+      <div style={{maxWidth: 800, margin: '0 auto'}}>
+        <Head><Brace charCode={123}/>Syntactor<Brace charCode={125}/></Head>
 
-          <Card style={{marginBottom: 10}}>
-            <Nav>
-              <Symbol>[</Symbol>
-              {links.map(([label, link], i) => (
-                <span key={label}>
+        <Card style={{marginBottom: 10}}>
+          <Nav>
+            <Symbol>[</Symbol>
+            {links.map(([label, link], i) => (
+              <span key={label}>
                   <a href={link}>{label}</a>
-                  {i + 1 < links.length && <Symbol>,</Symbol>}
+                {i + 1 < links.length && <Symbol>,</Symbol>}
                 </span>
-              ))}
-              <Symbol>]</Symbol>
-            </Nav>
-            <div>
-              An editor with two basic goals:
-              <ol>
-                <li>Manage syntax and code style (no syntax errors, no bikeshedding)</li>
-                <li>
-                  Common code transformations in just as many or less keystrokes,
-                  compared to other editors
-                </li>
-              </ol>
-              For now, it's only a JSON editor.
-            </div>
-          </Card>
+            ))}
+            <Symbol>]</Symbol>
+          </Nav>
+          <div>
+            An editor with two basic goals:
+            <ol>
+              <li>Manage syntax and code style (no syntax errors, no bikeshedding)</li>
+              <li>
+                Common code transformations in just as many or less keystrokes,
+                compared to other editors
+              </li>
+            </ol>
+            For now, it's only a JSON editor.
+          </div>
+        </Card>
 
-          <Card><Editor initiallyShowKeymap defaultValue={example}/></Card>
+        <Card><Editor initiallyShowKeymap defaultValue={example}/></Card>
       </div>
     );
   }
