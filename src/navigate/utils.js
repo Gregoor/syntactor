3eb2c888e63851880201c3a16654dadaacc0eb27
Gregor Weber <gregorwbr@gmail.com>
2018-02-19 18:27:37 +0100

    fix typing issue

--- src/navigate/utils.js
+++ src/navigate/utils.js
@@ -4,5 +4,5 @@ import type {ASTNode} from '../types';
 
 export function isNonEmptyCollection(node?: ASTNode) {
   return (isArrayExpression(node) || isObjectExpression(node))
-    && !((node.properties || node.elements): any).isEmpty();
+    && !((node: any).properties || (node: any).elements).isEmpty();
 }
